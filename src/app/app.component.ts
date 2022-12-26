1f8db28cd3f673e8d89d9a65b8b3ac480e58b8c7
ashnar2022 <ashnamearn2022@gmail.com>
2022-12-26 18:58:36 +0530

    todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'DAILY TO DO LIST';
+  list:any[]=[];
+
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item})
+    console.log(this.list);
+  }
+
+  removeTask(id:number){
+    console.log(id);
+    this.list=this.list.filter(item=>item.id!==id);
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,3 +16,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
